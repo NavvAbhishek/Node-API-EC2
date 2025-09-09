fb18b01ee0b391a1e165432ca816105b5ba6dbe0
NavvAbhishek <abi2000navi@gmail.com>
2025-09-10 00:40:34 +0530

    add order route

--- server.js
+++ server.js
@@ -24,6 +24,12 @@ app.use("/api/products", (req, res) => {
   });
 });
 
+app.use("/api/orders", (req, res) => {
+  return res.status(200).json({
+    message: "These are the orders",
+  });
+});
+
 //error handler
 app.use(errorHandler);
 
